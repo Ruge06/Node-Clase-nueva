42c4fa43daf7dece456978aa8da067af5a234b87
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-10-18 21:53:23 -0500

    Avance 18-10

---
+++ src/app/Componentes/fotos/fotos.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { fotos } from './fotos';
+
+@Component({
+  selector: 'fotos',
+  templateUrl: './fotos.component.html',
+  styleUrls: ['./fotos.component.css']
+})
+export class FotosComponent implements OnInit {
+
+  misfotos = fotos;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+

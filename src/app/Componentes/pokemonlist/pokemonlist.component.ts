711aea65e3111f7ef70eb2ec4fdfbf6914e80ccc
PC1-16\PC1-17 <sistemas@artesyletras.com.co>
2022-11-01 21:09:14 -0500

    Avance 1-11 2

--- src/app/Componentes/pokemonlist/pokemonlist.component.ts
+++ src/app/Componentes/pokemonlist/pokemonlist.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/servicios/pokemon.service';
 
 @Component({
-  selector: 'app-pokemonlist',
+  selector: 'app-pokemon-list',
   templateUrl: './pokemonlist.component.html',
   styleUrls: ['./pokemonlist.component.css']
 })
 export class PokemonlistComponent implements OnInit {
+  pokemon = {};
+  constructor(private pokemonService: PokemonService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {this.pokemonService.getPokemon().subscribe(respuesta => {
+    this.pokemon=respuesta.results;
+    console.log(this.pokemon);
+  })
   }
 
 }
--- src/app/servicios/pokemon.service.ts
+++ src/app/servicios/pokemon.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPokemon(): Observable<any>{
+    return this.http.get("https://pokeapi.co/api/v2/pokemon/?limit=200");
+  }
 }
